ac8f8e73942586792ae4983a91f1412099bbe9b4
Muhammad Umair <umair.nasir014@gmail.com>
2026-03-16 21:02:59 +0200

    reqs added

--- word2world/__init__.py
+++ word2world/__init__.py
@@ -1,7 +1,11 @@
-from .word2world import Word2World
 from .configs import Config
-from .agent import Word2WorldEnv, LLMAgent
 from .game_engine import DIRECTION_OFFSETS, Word2WorldGame, load_game_data
-from .utils import *
 from .fixers import *
 from .solvers import *
+
+try:
+    from .word2world import Word2World
+    from .agent import Word2WorldEnv, LLMAgent
+    from .utils import *
+except ImportError:
+    pass
